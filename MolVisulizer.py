39201eab34e4de28088a4c58c8eb37664943ea01
Jannis Wowra <janniswowra@gmx.de>
2024-04-21 22:08:19 +0200

    winkel richtig

--- MolVisulizer.py
+++ MolVisulizer.py
@@ -1,7 +1,10 @@
 from rdkit import Chem
 from rdkit.Chem import AllChem
 import plotly.graph_objs as go
-
+import numpy as np
+from rdkit import Chem
+from rdkit.Chem import AllChem
+import plotly.graph_objs as go
 class MoleculeVisualizer:
     def __init__(self, smiles):
         self.smiles = smiles
@@ -29,18 +32,26 @@ class MoleculeVisualizer:
                 'size': 30
             }
 
-        # Erstelle ein Dictionary für jede Bindung
         bonds_dict = {}
-        for bond in self.bonds:
+        for i, bond in enumerate(self.bonds):
             start_atom_idx = bond.GetBeginAtomIdx()
             end_atom_idx = bond.GetEndAtomIdx()
+            start_position = np.array(self.positions[start_atom_idx])
+            end_position = np.array(self.positions[end_atom_idx])
+            vector = end_position - start_position
+            length = np.linalg.norm(vector)
+            normalized_vector = vector / length if length != 0 else np.array([0,0,0])
+
             bonds_dict[(start_atom_idx, end_atom_idx)] = {
+                'index': i,
                 'start_atom_idx': start_atom_idx,
                 'end_atom_idx': end_atom_idx,
-                'start_position': self.positions[start_atom_idx],
-                'end_position': self.positions[end_atom_idx],
+                'start_position': start_position.tolist(),
+                'end_position': end_position.tolist(),
                 'color': 'gray',
-                'width': 5
+                'width': 5,
+                'length': length,
+                'normalized_vector': normalized_vector.tolist()
             }
 
         return atoms_dict, bonds_dict
@@ -76,11 +87,50 @@ class MoleculeVisualizer:
 
         return atoms_dict, bonds_dict
 
+    def calculate_bond_angles(self):
+        # Erstelle ein Dictionary für die Atome und Bindungen
+        atoms_dict, bonds_dict = self.generate_dicts()
+
+        angles_mat = np.zeros((len(bonds_dict), len(bonds_dict)))
+        # Gehe durch jedes Atom und berechne die Winkel zwischen den Bindungen
+        for atom_idx in atoms_dict:
+            connected_bonds = [bond_key for bond_key in bonds_dict if atom_idx in bond_key]
+
+            # Wenn weniger als zwei Bindungen vorhanden sind, kann kein Winkel berechnet werden
+            if len(connected_bonds) < 2:
+                continue
+
+            # Berechne die Winkel zwischen allen Bindungen, die am Atom ansetzen
+            for i in range(len(connected_bonds)):
+                for j in range(i + 1, len(connected_bonds)):
+                    bond1_key = connected_bonds[i]
+                    bond2_key = connected_bonds[j]
+                    start0 = bond1_key[0]
+                    end0 = bond1_key[1]
+                    start1 = bond2_key[0]
+                    end1 = bond2_key[1]
+                    connectedAtoms = set([start0, end0, start1, end1])
+
+                    if len(connectedAtoms) < 4:
+                        bond0 = bonds_dict[bond1_key]
+                        bond1 = bonds_dict[bond2_key]
+                        vec1 = np.array(bond0['normalized_vector'])
+                        vec2 = np.array(bond1['normalized_vector'])
+
+                        # Berechne den Winkel zwischen den Vektoren
+                        dot_product = np.dot(vec1, vec2)
+                        angle = np.arccos(np.clip(dot_product, -1.0, 1.0)) * 180 / np.pi
+
+                        idx0 = bond0['index']
+                        idx1 = bond1['index']
+                        angles_mat[idx0, idx1] = angle
+                        angles_mat[idx1, idx0] = angle
+        return angles_mat
+
     def find_adjacent_bond(self):
         atoms_dict = self.atoms_dict
         bonds_dict = self.bonds_dict
-
-
+        angle_matrix = self.calculate_bond_angles()
         for key in atoms_dict.keys():
             index = atoms_dict[key]['index']
             bondsCounter = 0
@@ -89,6 +139,7 @@ class MoleculeVisualizer:
                 if begin == index or end == index:
                     print(index, begin, end)
                     adjBonds[bondsCounter] = (begin, end)
+
                     bondsCounter += 1
             atoms_dict[key].update(adjBonds)
 
@@ -101,14 +152,15 @@ class MoleculeVisualizer:
             if hybridization == 'SP3' and bond_count == 2:
                 # ether
                 print(f"Atom {symbol}{key} mit SP3 Hybridisierung hat genau 2 Bindungen")
+                a = atom[0]
+                b = atom[1]
+
+                idx0 = bonds_dict[a]['index']
+                idx1 = bonds_dict[b]['index']
+                angle = angle_matrix[idx0, idx1]
+                print(f"Angle between bonds: {angle:.2f} degrees")
 
 
-                # Führe spezifische Aktionen hier aus
-            elif hybridization == 'SP2' and bond_count == 1:
-                print(f"Atom {key} mit SP2 Hybridisierung hat genau 1 Bindung")
-                # Führe spezifische Aktionen hier aus
-
-        #case depented action
     def create_3d_plot(self):
         atoms_dict, bonds_dict = self.generate_dicts()
         atom_hybridization, bond_types = self.analyze_molecule()
@@ -155,6 +207,6 @@ class MoleculeVisualizer:
         fig.show()
 
 # Beispiel zur Verwendung der Klasse
-smiles_string = "CCO"  # Beispiel SMILES für Ethanol
+smiles_string = "CO"  # Beispiel SMILES für Ethanol
 visualizer = MoleculeVisualizer(smiles_string)
 visualizer.create_3d_plot()
